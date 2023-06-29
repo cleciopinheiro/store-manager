d5e66ff704bd3c86ffed145e019534d13bc0b441
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-06-29 01:50:04 -0300

    o Avaliador do GitHub não está passando

--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -5,5 +5,6 @@ const app = express.Router();
 
 app.get('/products', productsController.getAll);
 app.get('/products/:id', productsController.getId);
+// app.post('/products', productsController.create);
 
 module.exports = app;
